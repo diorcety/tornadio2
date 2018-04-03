07eb0bf15e6630f4a080dee220c54186a840876e
Yann Diorcet <yann.diorcet@xenocs.com>
2018-04-03 15:04:52 +0200

    Use custom x version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except:
 
 setup(
     name='TornadIO2',
-    version='0.0.4',
+    version='0.0.4.dev0+x',
     author='Serge S. Koval',
     author_email='serge.koval@gmail.com',
     packages=['tornadio2'],
